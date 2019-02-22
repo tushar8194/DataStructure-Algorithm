6c457f99a3004e44e6f31b5fe45ae8ae6e99c2b2
TusharSinghal <tushar.singhal@target.com>
2019-02-22 20:04:51 +0530

    adding simple text editor

--- java/hackerrank/HRStackSimpleTextEditor.java
+++ java/hackerrank/HRStackSimpleTextEditor.java
@@ -9,27 +9,37 @@ public class HRStackSimpleTextEditor {
 
     public static void main(String[] args) {
 
-
-        Stack<Character> st = new Stack<>();
-
+        Stack<String> st = new Stack<>();
         Scanner sc =new Scanner(System.in);
 
         int numcases = sc.nextInt();
+        String currentString = "";
 
         while (numcases > 0){
-
             int instruct = sc.nextInt();
 
             switch (instruct){
                 case 1 :
+                    st.push(currentString);
+                    String append = sc.next();
+                    currentString = currentString + append;
                     break;
                 case 2 :
+                    st.push(currentString);
+                    int numChar = sc.nextInt();
+                    currentString = currentString.substring(0,currentString.length()-numChar);
                     break;
                 case 3 :
+                    int index = sc.nextInt();
+                    System.out.println(currentString.charAt(index - 1));
+                    break;
+                case 4 :
+                    currentString = st.pop();
                     break;
                 default:
                     break;
             }
+        numcases--;
         }
     }
 
