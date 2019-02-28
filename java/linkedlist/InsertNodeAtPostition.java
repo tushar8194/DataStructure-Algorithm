d61bff77eb61043bdbc09903753e57eab0d62a17
tushar8194 <tushar8194@gmail.com>
2019-03-01 01:24:04 +0530

    added linked list InsertNodeAtPostition

---
+++ java/linkedlist/InsertNodeAtPostition.java
@@ -0,0 +1,47 @@
+package linkedlist;
+
+public class InsertNodeAtPostition {
+
+
+
+    public static void intsertNodeAtPosition(Node head, String data, int position){
+        Node current = head;
+        Node prev = head;
+        int i=0;
+
+        Node node = new Node(data);
+
+        if(head == null){
+            head = node;
+        }else {
+            while (i<=position){
+                prev = current;
+                current = current.next;
+                i++;
+            }
+            prev.next = node;
+            node.next = current;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.addElementAtBegining("H");
+        list.addElementAtBegining("G");
+        list.addElementAtBegining("F");
+        list.addElementAtBegining("E");
+        list.addElementAtBegining("D");
+        list.addElementAtBegining("C");
+        list.addElementAtBegining("B");
+        list.addElementAtBegining("A");
+        list.printList();
+        System.out.println("------------------------------");
+
+        intsertNodeAtPosition(list.head,"TUSHAR",7);
+        list.printList();
+
+    }
+}
