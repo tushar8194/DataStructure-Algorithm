3b9fda47042e2f9327e8ee09cf28c50441e2a177
tushar8194 <tushar8194@gmail.com>
2019-11-24 13:36:10 +0530

    comapre strings scala

---
+++ scala/hackerrank/interview/compareString.scala
@@ -0,0 +1,25 @@
+package hackerrank.interview
+
+
+import scala.util.Sorting.quickSort
+object compareString {
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def compareStrings(firstString : String, secondString : String, thirdString : String) : String ={
+
+    val stringArray = new Array[String](3)
+    stringArray(0) = firstString
+    stringArray(1) = secondString
+    stringArray(2) = thirdString
+
+    quickSort(stringArray)
+
+    stringArray(0) + stringArray(1) + stringArray(2)
+
+  }
+
+}
